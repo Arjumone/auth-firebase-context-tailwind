e5eafb4b9f1b64c1dfee4ad03a4b37561e08f159
Arju Mone <121635205+Arjumone@users.noreply.github.com>
2023-05-05 21:37:55 +0600

    added auth email,passwprd context

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyClYB_f8bp1pZFX4BVgDgX1PWcIcD3TPwA",
+  authDomain: "auth-firebase-context-ta-418cc.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-418cc",
+  storageBucket: "auth-firebase-context-ta-418cc.appspot.com",
+  messagingSenderId: "1088035112578",
+  appId: "1:1088035112578:web:a6d622ba36011e4f67969b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
